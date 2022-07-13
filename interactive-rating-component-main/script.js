15ee932cc057c76864acab9e55a4d6304556ecc3
josemgfortunato <jose.fortunato@live.com>
2022-07-13 00:38:23 -0300

    primeira parte do CSS ok

---
+++ interactive-rating-component-main/script.js
@@ -0,0 +1,16 @@
+const oneEl = document.querySelector("one");
+const twoEl = document.querySelector("two");
+const threeEl = document.querySelector("three");
+const fourEl = document.querySelector("four");
+const fiveEl = document.querySelector("five");
+
+oneEl.addEventListener('click', select());
+twoEl.addEventListener('click', select());
+threeEl.addEventListener('click', select());
+fourEl.addEventListener('click', select());
+fiveEl.addEventListener('click', select());
+
+function select() {
+    
+}
+
