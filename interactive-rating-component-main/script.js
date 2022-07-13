39808bff57504add29ca8fb09ab5004ef58518fe
José <jose.fortunato@live.com>
2022-07-13 19:01:04 -0300

    new function

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,15 +1,22 @@
-const oneEl = document.querySelector("one");
-const twoEl = document.querySelector("two");
-const threeEl = document.querySelector("three");
-const fourEl = document.querySelector("four");
-const fiveEl = document.querySelector("five");
+const oneEl = document.querySelector("#one");
+const twoEl = document.querySelector("#two");
+const threeEl = document.querySelector("#three");
+const fourEl = document.querySelector("#four");
+const fiveEl = document.querySelector("#five");
 
-oneEl.addEventListener('click', () => {
-    oneEl.classList.add('mainbox__rating-numbers--selected');
+const elements = [oneEl, twoEl, threeEl, fourEl, fiveEl];
+
+elements.forEach(element => {
+    element.addEventListener('click', () => {    
+        if (element.className = 'mainbox__rating-numbers') {
+            element.classList.add('mainbox__rating-numbers--selected');
+        } else {
+            if (element.className = 'mainbox__rating-numbers--selected') {  
+                element.classList.add('mainbox__rating-numbers');  
+            }
+        }
+    }
+);
+    
 });
-// twoEl.addEventListener('click', select());
-// threeEl.addEventListener('click', select());
-// fourEl.addEventListener('click', select());
-// fiveEl.addEventListener('click', select());
-
 
