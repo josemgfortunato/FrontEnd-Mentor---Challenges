ef7171e9a05495259c812cdf987d8ba488404062
José <jose.fortunato@live.com>
2022-07-13 19:00:04 -0300

    js-newStuffs

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -4,13 +4,12 @@ const threeEl = document.querySelector("three");
 const fourEl = document.querySelector("four");
 const fiveEl = document.querySelector("five");
 
-oneEl.addEventListener('click', select());
-twoEl.addEventListener('click', select());
-threeEl.addEventListener('click', select());
-fourEl.addEventListener('click', select());
-fiveEl.addEventListener('click', select());
+oneEl.addEventListener('click', () => {
+    oneEl.classList.add('mainbox__rating-numbers--selected');
+});
+// twoEl.addEventListener('click', select());
+// threeEl.addEventListener('click', select());
+// fourEl.addEventListener('click', select());
+// fiveEl.addEventListener('click', select());
 
-function select() {
-    
-}
 
