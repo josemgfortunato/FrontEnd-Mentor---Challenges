b220ec8e75930cb42637670d96be7d6d907d550d
josemgfortunato <jose.fortunato@live.com>
2022-07-14 14:38:57 -0300

    projeto finalizado - adicionado função para trocar conteúdo da div principal

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,11 +1,15 @@
 
-const numbersEl = document.querySelectorAll(".mainbox__rating-numbers");
+const numbersEl = document.querySelectorAll(".mainbox__rating-numbers")
+const mainlEl = document.querySelector("#main")
+const submitEl = document.querySelector("#submit")
+
 
 
 numbersEl.forEach(element => {
     element.addEventListener('click', () => { 
     clean()    
     element.classList.add('mainbox__rating-numbers--selected')
+    ratingResult(element.innerText)
     })
 })
 
@@ -14,3 +18,27 @@ function clean() {
         number.classList.remove('mainbox__rating-numbers--selected')
     }
 )}
+
+// ratingResult()
+
+function ratingResult(number) {
+    submitEl.addEventListener('click', () => {
+        mainlEl.innerHTML = ""
+        mainlEl.innerHTML = `
+        <div class="mainbox__illustration">
+        <img src="./images/illustration-thank-you.svg" alt="Illustration Thank You">
+        </div>
+        <div class="mainbox__youselected">
+        <span class="mainbox__youselected-text">
+          You selected ${number} out of 5
+        </span>
+        </div>
+        <div class="mainbox__text">
+            <h1>Thank You!</h1>
+            <p> We appreciate you taking the time to give a rating
+                if you ever need more support, don't hesitate to get in touch!
+            </p>
+        </div>
+        `
+    })
+}
